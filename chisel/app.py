dcba59d7b7620c4b0d28209e8c586f3738e5f7e6
Craig Hobbs <craigahobbs@gmail.com>
2013-01-18 05:13:46 -0800

    Make application init method "public".  Also, fix application callRaw.

--- chisel/app.py
+++ chisel/app.py
@@ -122,42 +122,19 @@ class Application:
 
         # Initialize now if possible
         if configPath and scriptFilename:
-            self._init(None)
+            self.init(None)
 
     # WSGI entry point
     def __call__(self, environ, start_response):
 
         # Initialize, if necessary
-        self._init(environ)
+        self.init(environ)
 
         # Delegate to API application helper
         return self._api(environ, start_response)
 
-    # Create the action context object
-    def createContext(self, environ):
-
-        # Initialize, if necessary
-        self._init(environ)
-
-        # Do the work...
-        return self._createContext(environ)
-
-    def _createContext(self, environ):
-
-        # Create the logger
-        logger = logging.getLoggerClass()("")
-        logger.addHandler(logging.StreamHandler(environ.get("wsgi.errors")))
-        logger.setLevel(self._getLogLevel(self._config))
-
-        # Create the action context
-        ctx = Struct()
-        ctx.resources = self._resources
-        ctx.config = self._config.config
-        ctx.environ = environ
-        ctx.log = logger
-        return ctx
-
-    def _init(self, environ):
+    # Initialize the application - load configuration file, specs, and modules
+    def init(self, environ):
 
         with self._initLock:
 
@@ -207,6 +184,30 @@ class Application:
                     # Add the resource factory
                     self._resources[resource.name] = \
                         ResourceFactory(resource.name, resource.resourceString, resourceType[0])
+
+    # Create the action context object
+    def createContext(self, environ):
+
+        # Initialize, if necessary
+        self.init(environ)
+
+        # Do the work...
+        return self._createContext(environ)
+
+    def _createContext(self, environ):
+
+        # Create the logger
+        logger = logging.getLoggerClass()("")
+        logger.addHandler(logging.StreamHandler(environ.get("wsgi.errors")))
+        logger.setLevel(self._getLogLevel(self._config))
+
+        # Create the action context
+        ctx = Struct()
+        ctx.resources = self._resources
+        ctx.config = self._config.config
+        ctx.environ = environ
+        ctx.log = logger
+        return ctx
 
     # Configuration file specification
     configSpec = """\
@@ -346,27 +347,17 @@ struct ApplicationConfig
     def callRaw(self, method, url, data = None, environ = None):
 
         # WSGI environment - used passed-in environ if its complete
-        if (environ is not None and
-            "REQUEST_METHOD" in environ and
-            "PATH_INFO" in environ and
-            "wsgi.input" in environ and
-            "wsgi.errors" in environ):
-
-            _environ = environ
-
+        _environ = dict(environ) if environ else {}
+        _environ["REQUEST_METHOD"] = method
+        _environ["PATH_INFO"] = url
+        if data is not None:
+            _environ["wsgi.input"] = StringIO(data)
+            _environ["CONTENT_LENGTH"] = str(len(data))
         else:
-
-            _environ = dict(environ) if environ else {}
-            if "REQUEST_METHOD" not in _environ:
-                _environ["REQUEST_METHOD"] = method
-            if "PATH_INFO" not in _environ:
-                _environ["PATH_INFO"] = url
-            if "CONTENT_LENGTH" not in _environ and data is not None:
-                _environ["CONTENT_LENGTH"] = str(len(data))
-            if "wsgi.input" not in _environ:
-                _environ["wsgi.input"] = StringIO(data if data is not None else "")
-            if "wsgi.errors" not in _environ:
-                _environ["wsgi.errors"] = StringIO()
+            _environ["wsgi.input"] = StringIO()
+            if "CONTENT_LENGTH" in _environ:
+                del _environ["CONTENT_LENGTH"]
+        _environ["wsgi.errors"] = StringIO()
 
         # Call the action
         startResponseArgs = {}
