981660a1d3a2d43a9084099639891a0cb9d436e6
Craig Hobbs <craigh@zillow.com>
2013-06-12 19:01:46 -0700

    Bump version to 0.4.5

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ from setuptools import setup
 
 setup(
     name = 'chisel',
-    version = '0.4.4',
+    version = '0.4.5',
     author = 'Craig Hobbs',
     author_email = 'craigahobbs@gmail.com',
     description = ('JSON web APIs made dirt simple'),
