8ccd8d3e8ee14a4c73d251c67dcd31bdb581481c
Craig Hobbs <craigahobbs@gmail.com>
2014-11-15 10:51:45 -0800

    Bump version to 0.8.15

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ tests_require = []
 
 setup(
     name = 'chisel',
-    version = '0.8.14',
+    version = '0.8.15',
     author = 'Craig Hobbs',
     author_email = 'craigahobbs@gmail.com',
     description = ('JSON web APIs made dirt simple'),
