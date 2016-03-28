850456de4559d5bcef605f36a4ac2a1dc1ec6348
Craig Hobbs <craigahobbs@gmail.com>
2016-03-27 18:19:24 -0700

    add profiler option to __main__

--- chisel/__main__.py
+++ chisel/__main__.py
@@ -23,27 +23,44 @@
 import argparse
 from wsgiref.simple_server import make_server
 
+from chisel import Application, DocAction
+try:
+    from repoze.profile import ProfileMiddleware
+except ImportError:
+    pass
+
 
 def main():
 
     # Command line arguments
     parser = argparse.ArgumentParser(prog='python -m chisel')
-    parser.add_argument('file',
+    parser.add_argument('file', nargs='?',
                         help='a Python file that contains the WSGI application callable')
     parser.add_argument('-a', dest='application', metavar='NAME', default='application',
                         help='the name of the WSGI application callable (default is "application")')
     parser.add_argument('-p', type=int, dest='port', default=8080,
                         help='the WSGI service port (default is 8080)')
+    parser.add_argument('--profile', action='store_true', dest='profile',
+                        help='enable the profiler at /__profile__')
     args = parser.parse_args()
 
     # Execute WSGI application file
-    application_globals = {}
-    with open(args.file) as wsgi_file:
-        exec(compile(wsgi_file.read(), args.file, 'exec'), application_globals) # pylint: disable=exec-used
+    if args.file:
+        application_globals = {}
+        with open(args.file) as wsgi_file:
+            exec(compile(wsgi_file.read(), args.file, 'exec'), application_globals) # pylint: disable=exec-used
+        application = application_globals[args.application]
+    else:
+        application = Application()
+        application.add_request(DocAction())
+
+    # Wrap with profiler application?
+    if args.profile:
+        application = ProfileMiddleware(application)
 
     # Serve the WSGI application
     print('serving on port {0}...'.format(args.port))
-    make_server('', args.port, application_globals[args.application]).serve_forever()
+    make_server('', args.port, application).serve_forever()
 
 
 if __name__ == '__main__':
