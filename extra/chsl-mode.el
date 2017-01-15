7e7d422443dca226525a89ceec899310101b3950
Craig Hobbs <craigahobbs@gmail.com>
2017-01-15 11:21:08 -0800

    define chsl-mode as a generic-mode

--- extra/chsl-mode.el
+++ extra/chsl-mode.el
@@ -10,7 +10,6 @@
 ;;
 ;; (unless (package-installed-p 'chsl-mode)
 ;;   (let ((chsl-mode-file (make-temp-file "chsl-mode")))
-;;     (message "Installing chsl-mode")
 ;;     (url-copy-file "https://raw.githubusercontent.com/craigahobbs/chisel/master/extra/chsl-mode.el" chsl-mode-file t)
 ;;     (package-install-file chsl-mode-file)
 ;;     (delete-file chsl-mode-file)))
@@ -18,26 +17,39 @@
 
 ;;; Code:
 
-;; define several category of keywords
-(setq chsl-keywords '("action" "enum" "errors" "group" "input" "nullable" "optional" "output" "struct" "union"))
-(setq chsl-types '("bool" "date" "datetime" "float" "int" "object" "string" "uuid"))
+(require 'generic-x)
 
-;; generate regex string for each category of keywords
-(setq chsl-keywords-regexp (regexp-opt chsl-keywords 'words))
-(setq chsl-type-regexp (regexp-opt chsl-types 'words))
-
-;; create the list for font-lock.
-;; each category of keyword is given a particular face
-(setq chsl-font-lock-keywords
-      `(
-        (,chsl-type-regexp . font-lock-type-face)
-        (,chsl-keywords-regexp . font-lock-keyword-face)
-        ))
-
-;;;###autoload
-(define-derived-mode chsl-mode python-mode "chsl mode"
-  "Major mode for editing CHisel Specification Language"
-  (setq font-lock-defaults '((chsl-font-lock-keywords))))
+(define-generic-mode 'chsl-mode
+      '("#")
+      '(
+        "action"
+        "enum"
+        "errors"
+        "group"
+        "input"
+        "nullable"
+        "optional"
+        "output"
+        "struct"
+        "union"
+        )
+      (list
+       (cons
+        (regexp-opt
+         '(
+           "bool"
+           "date"
+           "datetime"
+           "float"
+           "int"
+           "object"
+           "string"
+           "uuid"
+           ) 'words) 'font-lock-type-face)
+        )
+      '(".chsl\\'")
+      nil
+      "Major mode for editing CHisel Specification Language")
 
 (provide 'chsl-mode)
 ;;; chsl-mode.el ends here
