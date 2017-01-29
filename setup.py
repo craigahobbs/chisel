2d88c7a43eaba3dcc5980e2b64344641f97c9abd
Craig Hobbs <craigahobbs@gmail.com>
2017-01-29 11:05:09 -0800

    drop support for Python 3.3

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setup(
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
