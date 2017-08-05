10f28512d2ae956c00b5781b04d742b97bc1ca77
Craig Hobbs <craigahobbs@gmail.com>
2017-08-05 09:50:37 -0700

    bump version to 0.9.49

--- chisel/__init__.py
+++ chisel/__init__.py
@@ -3,7 +3,7 @@
 # Licensed under the MIT License
 # https://github.com/craigahobbs/chisel/blob/master/LICENSE
 
-__version__ = '0.9.48'
+__version__ = '0.9.49'
 
 from .action import \
     action, \
