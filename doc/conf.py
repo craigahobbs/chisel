0e9c7a090bac3a292dbf9b7e0af765a3f4e4b4d8
Craig Hobbs <craigahobbs@gmail.com>
2017-08-19 07:50:30 -0700

    cleanup doc conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -17,9 +17,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+
 import os
 import sys
-# sys.path.insert(0, os.path.abspath('.'))
 
 # Import the chisel module.
 sys.path.insert(0, os.path.abspath('..'))
@@ -38,7 +38,7 @@ import chisel
 extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
-#templates_path = ['_templates']
+# templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -98,7 +98,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -171,6 +171,3 @@ texinfo_documents = [
      author, 'chisel', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
