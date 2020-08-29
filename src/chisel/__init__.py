9a8df02b6fe3345e769ea1143de22de7cbaac809
Craig Hobbs <craigahobbs@gmail.com>
2020-08-29 13:53:34 -0700

    chisel 0.9.111

--- src/chisel/__init__.py
+++ src/chisel/__init__.py
@@ -6,7 +6,7 @@ Chisel is a light-weight Python WSGI application framework with tools for buildi
 well-tested, schema-validated JSON web APIs.
 """
 
-__version__ = '0.9.110'
+__version__ = '0.9.111'
 
 from .action import \
     Action, \
