b6f5da94765867ca584f128ce8f40b84906c2cf2
Craig Hobbs <craigahobbs@gmail.com>
2021-04-07 20:01:25 -0700

    Revert "use doc alibaster theme"
    This reverts commit 018d0126837b89b9ca7a641f93e7bef10fdad801.

--- doc/conf.py
+++ doc/conf.py
@@ -53,14 +53,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
-html_theme_options = {
-    'font_family': 'arial',
-    'show_powered_by': False,
-    'link': '#004BAB',
-    'sidebar_link': '#004BAB',
-    'sidebar_link_underscore': '#ffffff'
-}
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
