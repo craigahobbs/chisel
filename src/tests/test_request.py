3c7f367c14a8edfe1e857c7c5b2a96cb968a751d
craigahobbs@gmail.com <Craig A. Hobbs>
2021-04-28 10:08:55 -0700

    improve test utility

--- src/tests/test_request.py
+++ src/tests/test_request.py
@@ -3,6 +3,7 @@
 
 # pylint: disable=missing-docstring
 
+from contextlib import contextmanager
 from http import HTTPStatus
 import os
 import sys
@@ -13,16 +14,21 @@ import unittest.mock
 from chisel import request, Application, Request, RedirectRequest, StaticRequest
 
 
+# Helper context manager to create a list of files in a temporary directory
+@contextmanager
 def create_test_files(file_defs):
     tempdir = TemporaryDirectory() # pylint: disable=consider-using-with
-    for path_parts, content in file_defs:
-        if isinstance(path_parts, str):
-            path_parts = [path_parts]
-        path = os.path.join(tempdir.name, *path_parts)
-        os.makedirs(os.path.dirname(path), exist_ok=True)
-        with open(path, 'w', encoding='utf-8') as file_:
-            file_.write(content)
-    return tempdir
+    try:
+        for path_parts, content in file_defs:
+            if isinstance(path_parts, str):
+                path_parts = [path_parts]
+            path = os.path.join(tempdir.name, *path_parts)
+            os.makedirs(os.path.dirname(path), exist_ok=True)
+            with open(path, 'w', encoding='utf-8') as file_:
+                file_.write(content)
+        yield tempdir.name
+    finally:
+        tempdir.cleanup()
 
 
 class TestRequest(TestCase):
