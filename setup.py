342606b740d9876799ef8c4cac195da37918819c
craigahobbs@gmail.com <Craig A. Hobbs>
2022-02-28 10:56:21 -0800

    add python 3.11 support

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ def main():
             'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: 3.9',
             'Programming Language :: Python :: 3.10',
+            'Programming Language :: Python :: 3.11',
             'Topic :: Internet :: WWW/HTTP :: WSGI',
             'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
             'Topic :: Software Development :: Libraries :: Application Frameworks'
