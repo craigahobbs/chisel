craigahobbs/mobstiq

lint test runners with bare -s

Makefile
@@ -38,7 +38,8 @@ clean:
 .PHONY: test-app
 commit: test-app
 test-app: $(DEFAULT_VENV_BUILD)
-	$(DEFAULT_VENV_BIN)/bare -x -m src/mobstiq/static/*.bare src/mobstiq/static/test/*.bare
+	$(DEFAULT_VENV_BIN)/bare -x -m src/mobstiq/static/*.bare src/mobstiq/static/test/test*.bare
+	$(DEFAULT_VENV_BIN)/bare -s -m src/mobstiq/static/test/runTests.bare
 	$(DEFAULT_VENV_BIN)/bare -d -m src/mobstiq/static/test/runTests.bare$(if $(TEST), -v vUnittestTest "'$(TEST)'")
 
 

craigahobbs/chisel

Makefile
@@ -42,7 +42,8 @@ doc:
 .PHONY: test-doc
 commit: test-doc
 test-doc: $(DEFAULT_VENV_BUILD)
-	$(DEFAULT_VENV_BIN)/bare -x -m src/chisel/static/*.bare src/chisel/static/test/*.bare
+	$(DEFAULT_VENV_BIN)/bare -x -m src/chisel/static/*.bare src/chisel/static/test/test*.bare
+	$(DEFAULT_VENV_BIN)/bare -s -m src/chisel/static/test/runTests.bare
 	$(DEFAULT_VENV_BIN)/bare -d -m src/chisel/static/test/runTests.bare$(if $(TEST), -v vUnittestTest "'$(TEST)'")
 
 